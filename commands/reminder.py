6f98e845a412acd54cefeb3b86320dd6faa55166
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2021-07-20 12:26:34 -0400

    debugging reminder

--- commands/reminder.py
+++ commands/reminder.py
@@ -237,40 +237,40 @@ class Reminder(commands.Cog):
                 while len(group_send) != 0:
                     base_reminder = group_send.pop(0)
                     user_mentions = []
-                    for reminder in group_send[:]:
-                        if group_reminder_exists(base_reminder, reminder):
-                            user = self.bot.get_user(reminder['user_id'])
-                            if user is None:
+                    for greminder in group_send[:]:
+                        if group_reminder_exists(base_reminder, greminder):
+                            guser = self.bot.get_user(greminder['user_id'])
+                            if guser is None:
                                 pass
-                            user_mentions.append(user.mention)
-                            group_send.remove(reminder)
+                            user_mentions.append(guser.mention)
+                            group_send.remove(greminder)
 
-                    delay = int(stime) - int(reminder['future_time'])
-                    embed = discord.Embed(
-                        title=f":bell:{' (Delayed)' if delay > self.SEND_DELAY_SECONDS else ''} Reminder! :bell:",
-                        colour=0x1abc9c
-                    )
-                    if delay > self.SEND_DELAY_SECONDS:
-                        embed.set_footer(
-                            text=f"""This was supposed to send {humanize_timedelta(seconds=delay)} ago.
-                                                        I might be having network or server issues, or perhaps I just started up.
-                                                        Sorry about that!"""
+                        gdelay = int(stime) - int(greminder['future_time'])
+                        gembed = discord.Embed(
+                            title=f":bell:{' (Delayed)' if delay > self.SEND_DELAY_SECONDS else ''} Reminder! :bell:",
+                            colour=0x1abc9c
                         )
-                    embed_name = f"From {reminder['future_timestamp']} ago:"
-                    if reminder['repeat']:
-                        embed_name = f"Repeating reminder every {humanize_timedelta(seconds=max(reminder['repeat'], 86400))}:"
-                    reminder_text = reminder['reminder']
-                    if len(reminder_text) > 900:
-                        reminder_text = reminder_text[:897] + "..."
-                    if reminder['jump_link']:
-                        reminder_text += f"\n\n[original message]({reminder['jump_link']})"
-                    embed.add_field(
-                        name=embed_name,
-                        value=reminder_text,
-                    )
-                    channel = self.bot.get_channel(reminder['channel_id'])
-                    await channel.send(f"{''.join(user_mentions)}")
-                    await channel.send(embed=embed)
+                        if gdelay > self.SEND_DELAY_SECONDS:
+                            gembed.set_footer(
+                                text=f"""This was supposed to send {humanize_timedelta(seconds=delay)} ago.
+                                                            I might be having network or server issues, or perhaps I just started up.
+                                                            Sorry about that!"""
+                            )
+                        gembed_name = f"From {greminder['future_timestamp']} ago:"
+                        if greminder['repeat']:
+                            gembed_name = f"Repeating reminder every {humanize_timedelta(seconds=max(greminder['repeat'], 86400))}:"
+                        greminder_text = reminder['reminder']
+                        if len(greminder_text) > 900:
+                            greminder_text = greminder_text[:897] + "..."
+                        if greminder['jump_link']:
+                            greminder_text += f"\n\n[original message]({greminder['jump_link']})"
+                        gembed.add_field(
+                            name=gembed_name,
+                            value=greminder_text,
+                        )
+                        channel = self.bot.get_channel(greminder['channel_id'])
+                        await channel.send(f"{''.join(user_mentions)}")
+                        await channel.send(embed=embed)
 
             await send_group_reminders(group_send)
 
