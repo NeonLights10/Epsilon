1cb87b7798e6293678375f7b697b56001b75384d
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2021-07-20 18:35:01 -0400

    Update reminder.py

--- commands/reminder.py
+++ commands/reminder.py
@@ -731,6 +731,7 @@ class Reminder(commands.Cog):
             post = {
                 'nid': nid,
                 'user_id': member.id,
+                'channel_id': ctx.channel.id,
                 'creation_date': time.time(),
                 'reminder': reminder_text,
                 'repeat': repeat,
