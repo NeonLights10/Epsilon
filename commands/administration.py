fd89cb4a4d409a234166d7fcb88ee2b5615f0b5d
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2021-07-31 17:40:25 -0400

    reset all override permissions for each channel

--- commands/administration.py
+++ commands/administration.py
@@ -592,6 +592,8 @@ class Administration(commands.Cog):
                     newpermissions = ctx.guild.roles[0].permissions
                     newpermissions.update(read_messages = False, send_messages = False)
                     await ctx.guild.roles[0].edit(reason='Enabling verification', permissions=newpermissions)
+                    for achannel in ctx.guild.text_channels:
+                        achannel.set_permissions(ctx.guild.roles[0], overwrite=None)
                     await channel.set_permissions(ctx.guild.roles[0], overwrite=discord.PermissionOverwrite(read_messages = True, add_reactions = True))
                     role = discord.utils.find(lambda r: r.name == 'Verified', ctx.guild.roles)
                     if role:
