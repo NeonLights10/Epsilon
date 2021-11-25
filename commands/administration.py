d2137d169a802c28730e036bc55767bd65b48050
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2021-11-24 23:09:45 -0500

    debug

--- commands/administration.py
+++ commands/administration.py
@@ -34,16 +34,18 @@ class Confirm(discord.ui.View):
     # We also send the user an ephemeral message that we're confirming their choice.
     @discord.ui.button(label="Yes", style=discord.ButtonStyle.green)
     async def confirm(self, button: discord.ui.Button, interaction: discord.Interaction):
-        #await interaction.response.send_message("Confirming", ephemeral=True)
-        button.disabled = True
+        for item in view.children:
+            item.disabled = True
+        await interaction.response.send_message("Confirming", ephemeral=True)
         self.value = True
         self.stop()
 
     # This one is similar to the confirmation button except sets the inner value to `False`
     @discord.ui.button(label="No", style=discord.ButtonStyle.red)
     async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+        for item in view.children:
+            item.disabled = True
         await interaction.response.send_message("Strike completed. User was not image muted.", ephemeral=True)
-        button.disabled = True
         self.value = False
         self.stop()
 
@@ -1222,16 +1224,16 @@ class Administration(commands.Cog):
 
             elif severity != '2' and ctx.guild.id == 432379300684103699:
                 view = Confirm(ctx)
-                await ctx.send(embed=gen_embed(title='Image Mute', content="Do you want to revoke image/external emote privileges?"), view=view)
+                bmessage = await ctx.send(embed=gen_embed(title='Image Mute', content="Do you want to revoke image/external emote privileges?"), view=view)
                 # Wait for the View to stop listening for input...
                 await view.wait()
                 if view.value is None:
                     log.info("View timed out")
-                    await ctx.edit(view=view)
+                    await bmessage.edit(view=view)
                     return
                 elif view.value:
                     log.info("Pressed Confirm Button")
-                    await ctx.edit(view=view)
+                    await bmessage.edit(view=view)
                     msg = await mutetime()
                     if msg:
                         mtime = convert_to_seconds(msg)
