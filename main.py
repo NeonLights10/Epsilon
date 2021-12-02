9460e756ed3d1de452b87e6a32e2ab41621d42c2
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2021-12-02 00:34:40 -0500

    more test

--- main.py
+++ main.py
@@ -285,12 +285,43 @@ def prefix(bot, message):
         prefix = default_prefix
     return prefix
 
-
 ####################
 
 log.info(f'Starting {NAME} {BOTVERSION}')
 
 bot = commands.Bot(command_prefix=prefix, intents=intents, case_insensitive=True)
+
+class PersistentEvent(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(
+        label="What's the current event?",
+        style=discord.ButtonStyle.green,
+        custom_id="persistent_view:currentevent",
+    )
+    async def currentevent(self, button: discord.ui.Button, interaction: discord.Interaction):
+        embed = gen_embed(
+            title='Current Status of EN Bandori',
+            content='v4.10.0 arrives <t:1638439200>.'
+        )
+        embed.set_image(
+            url='https://cdn.discordapp.com/attachments/611629664540295191/915809575331069962/Screenshot_20211201-223932_Google_Play_Store.png')
+        embed.add_field(name=f'Current Event',
+                        value=("Welcome to the Shrine\n"
+                               "<t:1638493200> to <t:1639033140>\n\n"
+                               "**Event Type**: Live Goals\n"
+                               "**Attribute**: Cool <:attrCool:432978841162612756>\n"
+                               "**Characters**: Arisa, Kaoru, Yukina, Mashiro, LAYER\n\n"
+                               "※The event period above is automatically converted to the timezone set on your system."),
+                        inline=False)
+        embed.add_field(name=f'Gacha',
+                        value=("2022 New Year Dream Festival Gacha\n"
+                               "Gorgeous New Year Parade Gacha [LIMITED]\n\n"
+                               "This list is subject to change. More information coming soon."),
+                        inline=False)
+        embed.set_footer(text='Last Updated 12/1/2021')
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 try:
     sys.stdout.write(f"\x1b]2;{NAME} {BOTVERSION}\x07")
@@ -609,35 +640,3 @@ bot.load_extension("commands.tiering")
 bot.load_extension("commands.reminder")
 bot.load_extension("commands.pubcord")
 bot.run(TOKEN)
-
-class PersistentEvent(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
-
-    @discord.ui.button(
-        label="What's the current event?",
-        style=discord.ButtonStyle.green,
-        custom_id="persistent_view:currentevent",
-    )
-    async def currentevent(self, button: discord.ui.Button, interaction: discord.Interaction):
-        embed = gen_embed(
-            title='Current Status of EN Bandori',
-            content='v4.10.0 arrives <t:1638439200>.'
-        )
-        embed.set_image(
-            url='https://cdn.discordapp.com/attachments/611629664540295191/915809575331069962/Screenshot_20211201-223932_Google_Play_Store.png')
-        embed.add_field(name=f'Current Event',
-                        value=("Welcome to the Shrine\n"
-                               "<t:1638493200> to <t:1639033140>\n\n"
-                               "**Event Type**: Live Goals\n"
-                               "**Attribute**: Cool <:attrCool:432978841162612756>\n"
-                               "**Characters**: Arisa, Kaoru, Yukina, Mashiro, LAYER\n\n"
-                               "※The event period above is automatically converted to the timezone set on your system."),
-                        inline=False)
-        embed.add_field(name=f'Gacha',
-                        value=("2022 New Year Dream Festival Gacha\n"
-                               "Gorgeous New Year Parade Gacha [LIMITED]\n\n"
-                               "This list is subject to change. More information coming soon."),
-                        inline=False)
-        embed.set_footer(text='Last Updated 12/1/2021')
-        await interaction.response.send_message(embed=embed, ephemeral=True)
