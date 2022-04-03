4c07348397f38fcf16d7c3be8bc149bb95eb339c
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2022-04-03 02:33:29 -0400

    update link

--- commands/pubcord.py
+++ commands/pubcord.py
@@ -150,9 +150,9 @@ class PersistentPlace(discord.ui.View):
     async def template(self, button: discord.ui.Button, interaction: discord.Interaction):
         embed = gen_embed(
             title='Template',
-            content=('Plans for expansion will continue after kasumi is established.'))
+            content=('Final plans.'))
         embed.set_image(
-            url='https://cdn.discordapp.com/attachments/959919689994240070/960048589340561418/unknown.png')
+            url='https://media.discordapp.net/attachments/959919689994240070/960062038149844992/unknown.png')
         embed.set_footer(text='r/place 2022')
         self.count += 1
         log.info(f'Quick Link Interaction {self.count}')
@@ -232,7 +232,7 @@ class Pubcord(commands.Cog):
                 await db.servers.update_one({"server_id": 432379300684103699},
                                             {"$set": {'prev_message': new_message.id}})
 
-    @tasks.loop(seconds=600.0)
+    @tasks.loop(seconds=300.0)
     async def check_place(self):
         document = await db.servers.find_one({"server_id": 432379300684103699})
         pubcord = self.bot.get_guild(432379300684103699)
