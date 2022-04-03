15726094ad64a747776747ffb7ea1d8f7a5f7b74
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2022-04-02 23:59:01 -0400

    update link

--- commands/pubcord.py
+++ commands/pubcord.py
@@ -232,7 +232,7 @@ class Pubcord(commands.Cog):
                 await db.servers.update_one({"server_id": 432379300684103699},
                                             {"$set": {'prev_message': new_message.id}})
 
-    @tasks.loop(seconds=10.0)
+    @tasks.loop(seconds=120.0)
     async def check_place(self):
         document = await db.servers.find_one({"server_id": 432379300684103699})
         pubcord = self.bot.get_guild(432379300684103699)
