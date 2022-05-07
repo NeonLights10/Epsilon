707ab882e35cd5ab4457acb85193e9c6e5b0c1d7
c0linw <41310013+c0linw@users.noreply.github.com>
2022-05-06 23:20:02 -0700

    Remove guild parameter in ModmailButton init

--- commands/modmail.py
+++ commands/modmail.py
@@ -36,7 +36,7 @@ class Confirm(discord.ui.View):
         self.stop()
 
 class ModmailButton(discord.ui.View):
-    def __init__(self, guild, bot):
+    def __init__(self, bot):
         super().__init__(timeout=None)
         self.bot = bot
 
