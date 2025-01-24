d6f9b7d6a53bbb065c04c6d8694a3872f8445e2f
NeonLights <8921618+NeonLights10@users.noreply.github.com>
2025-01-24 18:32:17 -0500

    remove temporary boot document edit

--- main.py
+++ main.py
@@ -181,21 +181,21 @@ async def check_document(guild, id):
     else:
         pass
         # Changeable to update old documents whenever a new feature/config is added
-        document = await db.servers.find_one({"server_id": id})
-        if document['log_channel']:
-            post = {'log_messages': [document['log_messages'], document['log_channel']],
-                    'log_joinleaves': [document['log_joinleaves'], document['log_channel']],
-                    'log_kbm': [document['log_kbm'], document['log_channel']],
-                    'log_strikes': [document['log_strikes'], document['log_channel']]}
-            await db.servers.update_one({"server_id": id},
-                                        {"$set": post})
-        else:
-            post = {'log_messages': [document['log_messages'], None],
-                    'log_joinleaves': [document['log_joinleaves'], None],
-                    'log_kbm': [document['log_kbm'], None],
-                    'log_strikes': [document['log_strikes'], None]}
-            await db.servers.update_one({"server_id": id},
-                                        {"$set": post})
+        #document = await db.servers.find_one({"server_id": id})
+        #if document['log_channel']:
+        #    post = {'log_messages': [document['log_messages'], document['log_channel']],
+        #            'log_joinleaves': [document['log_joinleaves'], document['log_channel']],
+        #            'log_kbm': [document['log_kbm'], document['log_channel']],
+        #            'log_strikes': [document['log_strikes'], document['log_channel']]}
+        #    await db.servers.update_one({"server_id": id},
+        #                                {"$set": post})
+        #else:
+        #    post = {'log_messages': [document['log_messages'], None],
+        #            'log_joinleaves': [document['log_joinleaves'], None],
+        #            'log_kbm': [document['log_kbm'], None],
+        #            'log_strikes': [document['log_strikes'], None]}
+        #    await db.servers.update_one({"server_id": id},
+        #                                {"$set": post})
 
 ##########
 
